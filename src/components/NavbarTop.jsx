dddeafa8171067be05f54db5410768d44ced63f5
Ryan Duong <ryan.duong@aol.co.uk>
2025-04-15 21:24:52 +0100

    Added feature that closes navbar on link click

--- src/components/NavbarTop.jsx
+++ src/components/NavbarTop.jsx
@@ -5,14 +5,19 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import {Link} from "react-router-dom";
 import {TypeAnimation} from "react-type-animation";
+import {useRef} from "react";
 
 function NavbarTop({ darkMode }) {
-  const [isOpen, isClosed] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); //state for Navbar.Collapse
   const [typingStatus, setTypingStatus] = useState("Initializing");
+
+  function collapseNav(){
+    setIsOpen(false);
+  }
 
   return(
     <>
-      <Navbar expand="lg" className={darkMode ? "navbar-custom-dark" : "navbar-custom-light"} fixed="top" data-bs-theme={darkMode ? "dark" : "light"}>
+      <Navbar expand="lg" className={darkMode ? "navbar-custom-dark" : "navbar-custom-light"} fixed="top" data-bs-theme={darkMode ? "dark" : "light"} expanded={isOpen}>
         <Container fluid>
           <Navbar.Brand
             style={{fontFamily:"Quicksand, sans-serif"}}
@@ -39,12 +44,12 @@ function NavbarTop({ darkMode }) {
               repeat={Infinity}
             />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setIsOpen(!isOpen)} />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto" style={{fontFamily:"Quicksand, sans-serif"}}>
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/about-me">About Me</Nav.Link>
-              <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+              <Nav.Link onClick={collapseNav} as={Link} to="/">Home</Nav.Link>
+              <Nav.Link onClick={collapseNav} as={Link} to="/about-me">About Me</Nav.Link>
+              <Nav.Link onClick={collapseNav} as={Link} to="/projects">Projects</Nav.Link>
               <NavDropdown title="Links" id="basic-nav-dropdown">
                 <NavDropdown.Item href="https://www.linkedin.com/in/ryan-duong-97b960328/">LinkedIn&nbsp;<i class="bi bi-linkedin"></i></NavDropdown.Item>
                 <NavDropdown.Item href="https://github.com/RyanDuong0">GitHub&nbsp;<i class="bi bi-github"></i></NavDropdown.Item>
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,11 +1,11 @@
 import NavbarTop from "../NavbarTop.jsx";
 import ThemeButton from "../ThemeButton.jsx";
 import WelcomeContent from "../WelcomeContent.jsx";
+import Carousel from "react-bootstrap/Carousel";
 
 function Projects({darkMode, toggleDarkMode}){
   return(
     <>
-      <WelcomeContent />
     </>
   );
 }
