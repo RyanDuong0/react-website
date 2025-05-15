faccfbe62af82b79dcefe5e91866e4999e7b676c
Ryan Duong <ryan.duong@aol.co.uk>
2025-05-15 19:44:50 +0100

    Fixed bug with no scrolling, due to react-under-construction

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,14 +1,11 @@
 import Container from "react-bootstrap/Container";
 import React, { useState } from "react";
-import TimelineStructure from "../TimelineStructure.jsx";
-import "react-under-construction/build/css/index.css";
 
 function Projects({ darkMode }) {
     return (
         <>
             <div className={darkMode ? "dark-mode" : "light-mode"}>
                 <Container className="mt-5">
-                    {/* <TimelineStructure darkMode={darkMode} /> */}
                     <div className={darkMode ? "dark-mode" : "light-mode"}>
                         <div style={{ paddingTop: 300 }}>
                             <h1 style={{ fontFamily: "Quicksand, sans-serif" }}>:(</h1>
